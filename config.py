a448758cd36afa5e76404e37a68d5d2e1484d00f
kaljo14 <78449182+kaljo14@users.noreply.github.com>
2023-03-15 12:46:31 +0200

    fixed failed split

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://test:test@192.168.99.245:5432/tester'
-    JWT_SECRET_KEY = 'your-secret-key-here'
--- models/readings.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Reading(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    meter_id = db.Column(db.Integer, db.ForeignKey('meter.id'), nullable=False)
-    reading_date = db.Column(db.Date)
-    reading_value = db.Column(db.Float)
